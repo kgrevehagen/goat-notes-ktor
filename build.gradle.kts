b53bc8808b8f1547155e0885aa753871a2833a90
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2025-09-24 14:10:38 +0200

    Remove Exposed and H2 dependencies from build configuration as they were not used

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-val exposed_version: String by project
-val h2_version: String by project
 val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -36,8 +34,6 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
     implementation("io.ktor:ktor-server-netty")
